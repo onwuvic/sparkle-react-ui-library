d20541f409264ad225aa5a5dda52aeb87defb0d8
Onwuzor Victor <onwuzorvictor@outlook.com>
2022-10-19 02:28:09 +0200

    feat: add button component fleshbone

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ButtonProps {
+    label: string
+}
+
+function Button(props: ButtonProps) {
+    return <button>{ props.label}</button>;
+}
+
+export default Button;
